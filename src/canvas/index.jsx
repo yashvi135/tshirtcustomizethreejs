71f48820372099bb5e0ac0c5840f922a13042cd6
Dhruvil2371 <109527681+Dhruvil2371@users.noreply.github.com>
2023-09-08 20:18:43 +0530

    changes in model

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -12,15 +12,15 @@ const CanvasModel = () => {
       shadows
       camera={{ position: [0, 0, 0],fov:25}}
       gl={{ preserveDrawingBuffer: true }}
-      style={{ width: "50%" }}
+      style={{ width:"60%" , height: "500px"}}
       className="w-full max-w-full h-full transition-all ease-in" 
     >
       <ambientLight intensity={0.5} />
-      <Environment preset="city" />
+      <Environment preset='city'  />
       <CameraRig>
          <Backdrop/> 
          <Center>
-        <Tshirt />
+        <Tshirt scale={16}/>
         </Center>
       </CameraRig>
     </Canvas>
